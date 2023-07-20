c0865c3ef25801b5adcbde0cb85c64328d11e6dd
JulitaZochowska <julita.zochowska@outlook.com>
2023-07-20 23:38:19 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,41 @@
 import Searchbar from './Searchbar';
-import ImageGallery from './ImageGallery';
-import ImageGalleryItem from './ImageGalleryItem';
+// import ImageGallery from './ImageGallery';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Searchbar />
-      <ImageGallery />
-      <ImageGalleryItem />
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  async fetchImages(query) {
+    const response = await fetch(
+      `https://pixabay.com/api/?q=cat&page=1&key=36881053-d0d1537e2fca48fbbc934d91b&image_type=photo&orientation=horizontal&per_page=12`
+    );
+    this.setState({ isLoading: true });
+
+    try {
+      const { page, per_page } = this.state;
+      const data = await response.json();
+      this.setState(prevState => ({ ...prevState, imageGallery: data }));
+    } catch (error) {
+      console.log('errr', error);
+    }
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Searchbar onSubmit={this.fetchImages} />
+        {/* <ImageGallery /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
