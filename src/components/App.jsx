863bd2359bd083cd32d30cdccab83d8cc46ce734
JulitaZochowska <julita.zochowska@outlook.com>
2023-07-21 20:36:05 +0200

    Button done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import Button from './Button';
+
 import React, { Component } from 'react';
 
 import css from './App.module.css';
 
 const INITIAL_STATE = {
+  searchInput: '',
   imageGallery: [],
   totalHits: 0,
   isLoading: false,
+  isLoadMoreButtonEnabled: false,
   page: 1,
   perPage: 12,
 };
@@ -15,19 +19,52 @@ const INITIAL_STATE = {
 class App extends Component {
   state = INITIAL_STATE;
 
-  fetchImages = async query => {
-    try {
-      this.setState({ ...INITIAL_STATE, isLoading: true }); //Ważne jest, aby przy każdym "nowym" wyszukiwaniu wyresetować wartości
-      const response = await fetch(
-        `https://pixabay.com/api/?q=${query}&page=${this.state.page}&key=36881053-d0d1537e2fca48fbbc934d91b&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`
-      );
-      const data = await response.json(); // Nie jestem pewien, czy używaliście tego w ten sposób - bazowałem na dokumentacji https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  updateSearchInput = query => {
+    if (query !== this.state.searchInput) {
+      this.setState(prevState => ({ ...INITIAL_STATE, searchInput: query }));
+    }
+  };
+
+  incrementPage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    console.log(this.state);
+    const { searchInput, isLoading, totalHits, page, perPage, imageGallery } =
+      this.state;
+    if (
+      (searchInput !== prevState.searchInput || page !== prevState.page) &&
+      searchInput.length > 0
+    ) {
+      const data = await this.fetchImages();
       this.setState(prevState => ({
-        ...prevState,
-        imageGallery: data.hits,
+        imageGallery: [...imageGallery, ...data.hits],
         totalHits: data.totalHits, // Przyda się do przycisku "load more" - będziesz porównywać ilość wyświetlonych obrazków z tym "totalHits" i na tej podstawie wyświetlać przycisk. ?
         // Najlepiej przygotuj sobie poprzednią pracę domową z galerią - bedą tu baaaaaaaardzo podobne mechanizmy
       }));
+    }
+
+    if (searchInput.length === 0) {
+      this.setState({ ...INITIAL_STATE });
+    }
+
+    const isLoadMoreButtonEnabled = totalHits > page * perPage && !isLoading;
+    // Porównujemy obecną wartość z poprzednią wartością, aby nie aktualizować
+    // stanu niepotrzebnie - w tej metodzie stworzyłoby to
+    // "nieskończony" ciąg wywołań (pętlę) - co zawiesi przeglądarkę
+    if (prevState.isLoadMoreButtonEnabled !== isLoadMoreButtonEnabled) {
+      this.setState({ isLoadMoreButtonEnabled: isLoadMoreButtonEnabled });
+    }
+  }
+
+  fetchImages = async () => {
+    try {
+      this.setState({ isLoading: true }); //Ważne jest, aby przy każdym "nowym" wyszukiwaniu wyresetować wartości
+      const response = await fetch(
+        `https://pixabay.com/api/?q=${this.state.searchInput}&page=${this.state.page}&key=36881053-d0d1537e2fca48fbbc934d91b&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`
+      );
+      return response.json(); // Nie jestem pewien, czy używaliście tego w ten sposób - bazowałem na dokumentacji https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     } catch (error) {
       console.error(error);
     } finally {
@@ -39,8 +76,14 @@ class App extends Component {
   render() {
     return (
       <div className={css.App}>
-        <Searchbar onSubmit={this.fetchImages} />
+        <Searchbar onSubmit={this.updateSearchInput} />
         <ImageGallery images={this.state.imageGallery} />
+        {this.state.isLoadMoreButtonEnabled ? (
+          <Button onClick={this.incrementPage} />
+        ) : (
+          ''
+        )}
+
         {/* TIP: Do rozmieszczenia kolejnych komponentów zobacz sobie
         App.module.css i niewykorzystane style - powinny dać Ci mały "hint" */}
 
